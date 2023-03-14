f3c38c309eda4521d24d8c38d42a46aecc28bdcc
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-03-13 19:05:05 -0500

    code funcionality to-do list

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
 new Vue ({
-    el: '#tasklist'
+    el: '#tasklist',
+    data: {
+        title: 'to do list',
+        tasks: [
+            {name: 'Today : Internal Meeting'},
+            {name: 'Tomorrow : Read a Book'},
+            {name: 'Daily :  Learn Programming'},
+            {name: 'Daily :  Morning Walk'},
+        ]
+    },
+    methods: {
+        newItem: function() {
+            if (!this.tasks.name) {
+                return
+            }
+            this.tasks.push ({
+                name: this.tasks.name,
+                del: ''
+            });
+            this.tasks.name = "";
+        },
+        delItem: function (task) {
+            this.tasks.splice(this.tasks.indexOf(task), 1)
+        }
+    }
 })
